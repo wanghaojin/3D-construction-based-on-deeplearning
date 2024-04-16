600424a3bcdc19c49588d3fda224ca87c836dbf5
WHJ <1316286198@qq.com>
2024-04-16 13:06:43 +0800

    model design

--- model.py
+++ model.py
@@ -26,3 +26,41 @@ class Linear(nn.Module):
     def forward(self, x):
         x = self.before(x)
         return self.after(self.fc(x))
+
+
+class Constructor(nn.Module):
+    def __init__(self, layers):
+        super(Constructor, self).__init__()
+        self.layers = []
+        for i in range(len(layers) - 1):
+            if i == 0:
+                self.layers.append(
+                    Linear(layers[i], layers[i + 1], before=None, after="Relu")
+                )
+            if i == len(layers) - 2:
+                self.layers.append(
+                    Linear(layers[i], layers[i + 1], before="Relu", after=None)
+                )
+            self.layers.append(
+                Linear(layers[i], layers[i + 1], before="Relu", after="Relu")
+            )
+        self.layers = nn.ModuleList(self.layers)
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+class ShadowConstruction(nn.Module):
+    def __init__(self, layers):
+        super(ShadowConstruction, self).__init__()
+        self.mlp1 = Constructor(layers)
+        self.mlp2 = Constructor(layers)
+        self.mlp3 = Constructor(layers)
+
+    def forward(self, img1, img2, img3):
+        height1 = self.mlp1(img1)
+        height2 = self.mlp2(img2)
+        height3 = self.mlp3(img3)
+        return height1, height2, height3
