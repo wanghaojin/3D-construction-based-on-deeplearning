8321e0c9fe642f889cdd91358167c779d50e279a
whj <whj@whjdeMacBook-Pro.local>
2024-04-19 12:50:44 +0800

    1

--- colored_construction/main.py
+++ colored_construction/main.py
@@ -16,7 +16,7 @@ num = xlsx['晶面'].isnull().idxmax()
 divide_list = []
 for i in range(num):
     R, G, B = xlsx['R'][i], xlsx['G'][i], xlsx['B'][i]
-    xyz = xlsx['晶面'][0][1:-1].split(' ')
+    xyz = xlsx['晶面'][i][1:-1].split(' ')
     x, y, z = [int(x) for x in xyz]
     divide_list.append(Divide([R, G, B], [x, y, z]))
 for i in range(m):
@@ -24,15 +24,26 @@ for i in range(m):
         R_img = image[i][j][2]
         G_img = image[i][j][1]
         B_img = image[i][j][0]
-        RGB_img = (R_img, G_img, B_img)
+        RGB_img = [R_img, G_img, B_img]
+        # if(RGB_img != [40,30,170]):
+        #     print(RGB_img)
         for divide in divide_list:
+            # if divide.RGB != [40,30,170]:
+                # print(divide.RGB)
+                # print(divide.n)
             if divide.RGB == RGB_img:
                 height_map[i][j].update(divide.n)
+                # if divide.n != [1,0,1]:
+                #     print(divide.n)
                 break
         if j == 0:
             continue
-        if height_map[i][j].n == None: 
+        if height_map[i][j].n == None or height_map[i][j-1].n == None: 
             height_map[i][j].update_height(height_map[i][j-1].height)
             continue
         height_map[i][j].update_height(height_map[i][j-1].height+calcualte_height(height_map[i][j-1].n, height_map[i][j].n))
 plot_height_map(height_map)
+for i in range(m):
+    for j in range(n):
+        if(height_map[i][j].n != [1,0,1] and height_map[i][j].n!= None):
+            print(height_map[i][j].n)
--- colored_construction/utils_col.py
+++ colored_construction/utils_col.py
@@ -17,9 +17,19 @@ class Divide:
         self.n = n
 
 def calcualte_height(n0,n1):
-    a0 ,b0, c0 = n0[0],n0[1],n0[2]
-    a1, b1, c1 = n1[0],n1[1],n1[2]
-    delta_h = math.sqrt((a0*a0 + b0*b0 + c0*c0)*(a1*a1 + b1*b1 + c1*c1) - (a0*a1 + b0*b1 + c0*c1)*(a0*a1 + b0*b1 + c0*c1))/(a0*a1 + b0*b1 + c0*c1)
+    a0 = n0[0]
+    b0 = n0[1]
+    c0 = n0[2]
+    a1 = n1[0]
+    b1 = n1[1]
+    c1 = n1[2]
+    if n0 != n1:
+        delta_h = math.sqrt(1-((a0*a1 + b0*b1 + c0*c1) / (math.sqrt(a0**2 + b0**2 + c0**2) * math.sqrt(a1**2 + b1**2 + c1**2)))**2)
+    if n0 == n1:
+        c0 = 0
+        c1 = 0
+        delta_h = (a0*a1 + b0*b1 + c0*c1) / (math.sqrt(a0**2 + b0**2 + c0**2) * math.sqrt(a1**2 + b1**2 + c1**2))
+
     return delta_h
 
 def plot_height_map(height_map):
